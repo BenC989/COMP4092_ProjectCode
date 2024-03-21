311d93a38f8de69a33229357078e47c35251f6c1
BenC989 <bencavanagh03@hotmail.com>
2024-03-21 18:34:14 +1100

    Successfully iterate over all folders and files

--- Testing/FileReadingTest.java
+++ Testing/FileReadingTest.java
@@ -4,15 +4,24 @@ public class FileReadingTest {
 
     public static void main(String[] args) {
         try {
-            File f = new File("C:\\Users\\User\\Documents\\COMP4092_ProjectCode\\Testing\\Files");
-
-            File[] files = f.listFiles();
-
-            System.out.println("Files are:"); 
+            File participantsFolder = new File("C:\\Users\\User\\Documents\\COMP4092_ProjectCode\\Testing\\Files\\Participants");
+            File[] participants = participantsFolder.listFiles();
   
-            // Display the names of the files 
-            for (int i = 0; i < files.length; i++) { 
-                System.out.println(files[i].getName()); 
+            // Iterate Participants folder (containing the participants)
+            for (int i = 0; i < participants.length; i++) { 
+                System.out.println(participants[i].getName()); 
+
+                // Iterate Participants -> Participant folder (containing the activities)
+                File[] participantActivities = participants[i].listFiles();
+                for (int j = 0; j < participantActivities.length; j++) {
+                    System.out.println("        " + participantActivities[j].getName());
+
+                    // Iterate Participants -> Participant -> Activity (containing the images)
+                    File[] participantActivityImages = participantActivities[j].listFiles();
+                    for (int k = 0; k < participantActivityImages.length; k++) {
+                        System.out.println("                " + participantActivityImages[k].getName());
+                    }
+                }
             } 
         }
         catch (Exception e) {
