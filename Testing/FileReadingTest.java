50941b45e30fa0811a846ab7d5b97895f32f1678
BenC989 <bencavanagh03@hotmail.com>
2024-03-22 20:30:53 +1100

    Created table for participant activities and times

--- Testing/FileReadingTest.java
+++ Testing/FileReadingTest.java
@@ -2,21 +2,36 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class FileReadingTest {
+    static ArrayList<Record> tableRecords = new ArrayList<>();
+    static FileWriter writer;
 
     /*
      * This is the main function used to execute the program
      */
     public static void main(String[] args) {
-        try {
-            // Initialise variables
-            FileWriter writer = new FileWriter("Data_Storage.txt");
-            ArrayList<Record> tableRecords = new ArrayList<>();
 
-            // Write table headings in data storage file
-            writeHeadings(writer);
+        // Initialise the file writer
+        try {
+            writer = new FileWriter("Data_Storage.txt");
+        }
+        catch (Exception e) {
+            System.err.println("Error! " + e.getMessage()); 
+        }
+
+        // Tabularise the data from the images
+        tabulariseData();
+
+        // Make an example call to participant activities function
+        participantActivities(writer, "Participant_01", tableRecords);
+    }
+
+    public static void tabulariseData() {
+        try {
+            // Write image data table headings in data storage file
+            writeImageTableHeadings(writer);
 
             // File participantsFolder = new File("C:\\Users\\User\\Documents\\COMP4092_ProjectCode\\Testing\\Files\\Participants");
-            File participantsFolder = new File("C:\\Users\\benca\\Documents\\COMP4092_ProjectCode\\Testing\\Files\\Participants");
+            File participantsFolder = new File("C:\\Users\\User\\Documents\\COMP4092_ProjectCode\\Testing\\Files\\Participants");
             File[] participants = participantsFolder.listFiles();
   
             // Iterate Participants folder (containing the participants)
@@ -58,22 +73,22 @@ public class FileReadingTest {
                         tableRecords.add(record);
 
                         // Write all data in data storage file
-                        writeImageID(writer, imageID);
-                        writeParticipantID(writer, participantID);
-                        writeDay(writer, day);
-                        writeMonth(writer, month);
-                        writeYear(writer, year);
-                        writeHour(writer, hour);
-                        writeMinute(writer, minute);
-                        writeSecond(writer, second);
-                        writeActivity(writer, activity);
+                        writeImageTableImageID(writer, imageID);
+                        writeImageTableParticipantID(writer, participantID);
+                        writeImageTableDay(writer, day);
+                        writeImageTableMonth(writer, month);
+                        writeImageTableYear(writer, year);
+                        writeImageTableHour(writer, hour);
+                        writeImageTableMinute(writer, minute);
+                        writeImageTableSecond(writer, second);
+                        writeImageTableActivity(writer, activity);
                     }
                 }
             }
-            writer.close();
 
-            // Example call to participant activities function
-            participantActivities(participants[0].getName(), tableRecords);
+            // Complete the table
+            writer.write("|----------|----------------|----------|--------|----------------|");
+            writer.write("\n \n \n \n");
         }
         catch (Exception e) {
             System.err.println("Error! " + e.getMessage()); 
@@ -81,9 +96,81 @@ public class FileReadingTest {
     }
 
     /*
-     * This function writes each of the table headings in data storage file
+     * This function considers one participant and calculates the time duration for each of their activities
      */
-    public static void writeHeadings(Writer writer) {
+    public static ArrayList<Activity> participantActivities(Writer writer, String participant, ArrayList<Record> tableRecords) {
+
+        // Store the participant's activities
+        ArrayList<Activity> participantActivities = new ArrayList<>();
+
+        try {
+            // Define variables
+            int index = 0;
+            boolean newActivity = true;
+            Activity activity = new Activity();
+
+            // Write participant activity data table headings in data storage file
+            writeActivityTableHeadings(writer);
+
+            // Find the records for the specific participant
+            while ((tableRecords.get(index).participantID.equals(participant)) == false) {
+                index++;
+            }
+
+            // Get all the records for this specific participant
+            while (tableRecords.get(index).participantID.equals(participant)) {
+
+                // When there a new activity is found
+                if (newActivity == true) {
+                    activity.name = tableRecords.get(index).activity;
+                    activity.startHour = tableRecords.get(index).hour;
+                    activity.startMinute = tableRecords.get(index).minute;
+                    activity.startSecond = tableRecords.get(index).second;
+                    newActivity = false;
+                }
+                else {
+
+                    // When it's the end of an activity
+                    if (((tableRecords.get(index).activity).equals(tableRecords.get(index - 1).activity)) == false) {
+                        activity.endHour = tableRecords.get(index - 1).hour;
+                        activity.endMinute = tableRecords.get(index - 1).minute;
+                        activity.endSecond = tableRecords.get(index - 1).second;
+
+                        // Calculate the activity time duration in minutes
+                        activity.duration = 
+                            (60*(activity.endHour - activity.startHour)) + (activity.endMinute - activity.startMinute);
+
+                        // Add this record to the participant's activity list
+                        participantActivities.add(activity);
+
+                        // Write all data in data storage file
+                        writeActivityTableParticipantID(writer, participant);
+                        writeActivityTableActivity(writer, activity.name);
+                        writeActivityTableDuration(writer, activity.duration);
+
+                        // Identify that a new activity has begun
+                        activity = new Activity();
+                        newActivity = true;
+                        index--;
+                    }
+                }
+                index++;
+            }
+
+            // Complete the table
+            writer.write("|----------------|----------------|----------|");
+            writer.close();
+        }
+        catch (Exception e) {
+            System.err.println("Error! " + e.getMessage()); 
+        }
+        return participantActivities;
+    }
+
+    /*
+     * This function writes each of the Image Table headings in data storage file
+     */
+    public static void writeImageTableHeadings(Writer writer) {
         try {
             writer.write("|----------|----------------|----------|--------|----------------|");
             writer.write("\n");
@@ -98,9 +185,9 @@ public class FileReadingTest {
     }
 
     /*
-     * This function writes the ImageID data in the data storage file
+     * This function writes the ImageID data in the Image Table in the data storage file
      */
-    public static void writeImageID(Writer writer, String imageID) {
+    public static void writeImageTableImageID(Writer writer, String imageID) {
         try {
             writer.write("|" + imageID);
             writer.write("       |");
@@ -111,9 +198,9 @@ public class FileReadingTest {
     }
 
     /*
-     * This function writes the ParticipantID data in the data storage file
+     * This function writes the ParticipantID data in the Image Table in the data storage file
      */
-    public static void writeParticipantID(Writer writer, String participantID) {
+    public static void writeImageTableParticipantID(Writer writer, String participantID) {
         try {
             writer.write(participantID);
         }
@@ -123,9 +210,9 @@ public class FileReadingTest {
     }
 
     /*
-     * This function writes the Day date data in the data storage file
+     * This function writes the Day date data in the Image Table in the data storage file
      */
-    public static void writeDay(Writer writer, int day) {
+    public static void writeImageTableDay(Writer writer, int day) {
         try {
             writer.write("  |");
             if (day < 10) {
@@ -141,9 +228,9 @@ public class FileReadingTest {
     }
 
     /*
-     * This function writes the Month date data in the data storage file
+     * This function writes the Month date data in the Image Table in the data storage file
      */
-    public static void writeMonth(Writer writer, int month) {
+    public static void writeImageTableMonth(Writer writer, int month) {
         try {
             writer.write("/");
             if (month < 10) {
@@ -159,9 +246,9 @@ public class FileReadingTest {
     }
 
     /*
-     * This function writes the Year date data in the data storage file
+     * This function writes the Year date data in the Image Table in the data storage file
      */
-    public static void writeYear(Writer writer, int year) {
+    public static void writeImageTableYear(Writer writer, int year) {
         try {
             writer.write("/");
             writer.write("" + year);
@@ -173,9 +260,9 @@ public class FileReadingTest {
     }
 
     /*
-     * This function writes the Hour time data in the data storage file
+     * This function writes the Hour time data in the Image Table in the data storage file
      */
-    public static void writeHour(Writer writer, int hour) {
+    public static void writeImageTableHour(Writer writer, int hour) {
         try {
             if (hour < 10) {
                 writer.write("0" + hour);
@@ -191,9 +278,9 @@ public class FileReadingTest {
     }
 
     /*
-     * This function writes the Minute time data in the data storage file
+     * This function writes the Minute time data in the Image Table in the data storage file
      */
-    public static void writeMinute(Writer writer, int minute) {
+    public static void writeImageTableMinute(Writer writer, int minute) {
         try {
             if (minute < 10) {
                 writer.write("0" + minute);
@@ -209,9 +296,9 @@ public class FileReadingTest {
     }
 
     /*
-     * This function writes the Second time data in the data storage file
+     * This function writes the Second time data in the Image Table in the data storage file
      */
-    public static void writeSecond(Writer writer, int second) {
+    public static void writeImageTableSecond(Writer writer, int second) {
         try {
             if (second < 10) {
                 writer.write("0" + second);
@@ -227,9 +314,9 @@ public class FileReadingTest {
     }
 
     /*
-     * This function writes the Activity data in the data storage file
+     * This function writes the Activity data in the Image Table in the data storage file
      */
-    public static void writeActivity(Writer writer, String activity) {
+    public static void writeImageTableActivity(Writer writer, String activity) {
         try {
             writer.write(activity);
             for (int l = (16-(16-(activity.length()))); l < 16; l++) {
@@ -243,46 +330,69 @@ public class FileReadingTest {
     }
 
     /*
-     * This function considers one participant and calculates the time duration for each of their activities
+     * This function writes each of the table headings in the Activity Table in the data storage file
      */
-    public static ArrayList<Activity> participantActivities(String participant, ArrayList<Record> tableRecords) {
-        // Define variables
-        ArrayList<Activity> participantActivities = new ArrayList<>();
-        int index = 0;
-        boolean newActivity = true;
-        Activity activity = new Activity();
-
-        while (tableRecords.get(index).participantID.equals(participant)) {
-
-            if (newActivity == true) {
-                activity.name = tableRecords.get(index).activity;
-                activity.startHour = tableRecords.get(index).hour;
-                activity.startMinute = tableRecords.get(index).minute;
-                activity.startSecond = tableRecords.get(index).second;
-                newActivity = false;
-            }
-            else {
-                if (((tableRecords.get(index).activity).equals(tableRecords.get(index - 1).activity)) == false) {
-                    activity.endHour = tableRecords.get(index - 1).hour;
-                    activity.endMinute = tableRecords.get(index - 1).minute;
-                    activity.endSecond = tableRecords.get(index - 1).second;
-
-                    activity.duration = 
-                        (60*(activity.endHour - activity.startHour)) + (activity.endMinute - activity.startMinute);
-
-                    participantActivities.add(activity);
-                    activity = new Activity();
-                    newActivity = true;
-                    index--;
-                }
-            }
-
-            index++;
+    public static void writeActivityTableHeadings(Writer writer) {
+        try {
+            writer.write("|----------------|----------------|----------|");
+            writer.write("\n");
+            writer.write("| Participant ID | Activity Class | Duration |");
+            writer.write("\n");
+            writer.write("|----------------|----------------|----------|");
+            writer.write("\n");
         }
-
-        for (int i = 0; i < participantActivities.size(); i++) {
-            System.out.println(participantActivities.get(i).name + " - " + participantActivities.get(i).duration);
+        catch (Exception e) {
+            System.err.println("Error! " + e.getMessage()); 
         }
-        return participantActivities;
+    }
+
+    /*
+     * This function writes the ParticipantID data in the Activity Table in the data storage file
+     */
+    public static void writeActivityTableParticipantID(Writer writer, String participantID) {
+        try {
+            writer.write("|" + participantID);
+            writer.write("  |");
+        }
+        catch (Exception e) {
+            System.err.println("Error! " + e.getMessage()); 
+        }
+    }
+
+    /*
+     * This function writes the Activity data in the Activity Table in the data storage file
+     */
+    public static void writeActivityTableActivity(Writer writer, String activity) {
+        try {
+            writer.write(activity);
+            for (int l = (16-(16-(activity.length()))); l < 16; l++) {
+                writer.write(" ");
+            }
+            writer.write("|");
+        }
+        catch (Exception e) {
+            System.err.println("Error! " + e.getMessage()); 
+        }
+    }
+
+    /*
+     * This function writes the Duration data in the Activity Table in the data storage file
+     */
+    public static void writeActivityTableDuration(Writer writer, int duration) {
+        try {
+            String answer = "";
+            if (duration < 100) {
+                answer = answer + 0;
+            }
+            if (duration < 10) {
+                answer = answer + 0;
+            }
+            answer = answer + duration;
+            writer.write(answer);
+            writer.write(" mins  |\n");
+        }
+        catch (Exception e) {
+            System.err.println("Error! " + e.getMessage()); 
+        }
     }
 }
